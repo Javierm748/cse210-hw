68906d065a699959c6b715d91f5e1d383ef3e5e6
Javierm748 <martinezjavier748@gmail.com>
2024-05-01 20:04:39 -0600

    Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+
+        while (userNumber != 0)
+        {
+            Console.WriteLine("Type a series of numbers, when you are done type 0");
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+
+            if (userNumber !=0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+    //    part 1: Compute the sum
+    int sum = 0;
+    foreach (int number in numbers)
+    {
+        sum += number;
+    }
+    Console.WriteLine($"The sum is: {sum}");
+
+    // Part 2: compute the average
+    float average = ((float)sum) / numbers.Count;
+    Console.WriteLine($"The average is: {average}");
+
+    // Part 3: Find the max
+
+
+    int max = numbers[0];
+
+    foreach (int number in numbers)
+    {
+        if (number > max)
+        {
+            max = number;
+        }
+    }
+    Console.WriteLine($"The max is: {max}");
+
     }
 }
