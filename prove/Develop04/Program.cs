6f09576be679c6ebe2238ebe75b2803fcbd8648c
Javierm748 <martinezjavier748@gmail.com>
2024-06-15 15:57:14 -0600

    delevop04

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,35 @@
 using System;
 
-class Program
+public abstract class MindfulnessActivity
 {
-    static void Main(string[] args)
+    protected int Duration { get; set; }
+
+    public void StartMessage(string activityName, string description)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        Console.WriteLine($"Starting {activityName} Activity");
+        Console.WriteLine(description);
+        Console.Write("Enter duration in seconds: ");
+        Duration = int.Parse(Console.ReadLine());
+        Console.WriteLine("Prepare to begin...");
+        PauseWithAnimation(5);
     }
+
+    public void EndMessage(string activityName)
+    {
+        Console.WriteLine($"Good job! You have completed the {activityName} activity.");
+        Console.WriteLine($"You spent {Duration} seconds on this activity.");
+        PauseWithAnimation(5);
+    }
+
+    public void PauseWithAnimation(int seconds)
+    {
+        for (int i = 0; i < seconds; i++)
+        {
+            Console.Write(".");
+            System.Threading.Thread.Sleep(1000);
+        }
+        Console.WriteLine();
+    }
+
+    public abstract void PerformActivity();
 }
